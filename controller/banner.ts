05bb38d509b25c51266aa21b8598e2dc05e6636b
gump <359013988@qq.com>
2024-11-26 10:33:41 +0800

    feat: 清理文件

--- controller/banner.ts
+++
@@ -1,9 +0,0 @@
-import { prefix, get } from '../requestDecorator'
-import bannerList from '../mockdb/bannerList'
-@prefix('/banner')
-export default class User {
-  @get('/getBanner')
-  async getBanner() {
-    return bannerList
-  }
-}
